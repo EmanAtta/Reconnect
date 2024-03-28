5c76e6458aa6b606b2dfeefe42f1643799ecf66e
EslamAgm <154402132+EslamAgm@users.noreply.github.com>
2024-03-28 17:14:00 +0200

    profile

--- reconnect/lib/Views/main.dart
+++ reconnect/lib/Views/main.dart
@@ -20,15 +20,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/get_core.dart';
 
-void main() async{
- WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then((value) =>Get.put(Authentication()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((value) => Get.put(Authentication()));
 
   runApp(
     const Reconnect(),
   );
 }
-
 
 class Reconnect extends StatelessWidget {
   const Reconnect({Key? key}) : super(key: key);
@@ -36,33 +36,28 @@ class Reconnect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const WelcomPage(),
-     //initialRoute: '/',
-      getPages: [
-        GetPage(name: '/', page: () => const WelcomPage()),
-        GetPage(name: '/login', page: () => const login()),
-        GetPage(name: '/signup', page: () => const signup()),
-        GetPage(name: '/forget', page: () => const forgetBassword()),
-        GetPage(name: '/reset', page: () => const resetPassword()),
-        GetPage(name: '/verfication', page: () => const verfiyCode()),
-        GetPage(name: '/home', page: () => const Home()),
-        GetPage(name: '/pick_image', page: () => const ImagePage()),
-       
-        GetPage(name: '/profile', page: () => const Profile()),
-        GetPage(name: '/settings', page: () => Settings()),
-        GetPage(name: '/privacy', page:()=>const Privacy()),
-        GetPage(name: '/about', page:()=>const About()),
-        GetPage(name: '/pay', page: () => const Pay()),
-        //GetPage(name: '/post', page: () => const Post()),
-        GetPage(name: '/edit', page: () => const Edit()),
-        GetPage(name: '/changepassword', page: () => const Change_Password()),
-        GetPage(name: '/welcome', page: () => const WelcomPage()),
+        debugShowCheckedModeBanner: false,
+        home: const WelcomPage(),
+        //initialRoute: '/',
+        getPages: [
+          GetPage(name: '/', page: () => const WelcomPage()),
+          GetPage(name: '/login', page: () => const login()),
+          GetPage(name: '/signup', page: () => const signup()),
+          GetPage(name: '/forget', page: () => const forgetBassword()),
+          GetPage(name: '/reset', page: () => const resetPassword()),
+          GetPage(name: '/verfication', page: () => const verfiyCode()),
+          GetPage(name: '/home', page: () => const Home()),
+          GetPage(name: '/pick_image', page: () => const ImagePage()),
 
-        
-       
-        
-      ]
-    );
+          GetPage(name: '/profile', page: () => const profile()),
+          GetPage(name: '/settings', page: () => Settings()),
+          GetPage(name: '/privacy', page: () => const Privacy()),
+          GetPage(name: '/about', page: () => const About()),
+          GetPage(name: '/pay', page: () => const Pay()),
+          //GetPage(name: '/post', page: () => const Post()),
+          GetPage(name: '/edit', page: () => const Edit()),
+          GetPage(name: '/changepassword', page: () => const Change_Password()),
+          GetPage(name: '/welcome', page: () => const WelcomPage()),
+        ]);
   }
 }
--- reconnect/lib/Views/profile.dart
+++ reconnect/lib/Views/profile.dart
@@ -1,18 +1,99 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reconnect/Views/color.dart';
 
-class Profile extends StatelessWidget {
-  const Profile({super.key});
+class profile extends StatefulWidget {
+  const profile({super.key});
+
+  @override
+  State<profile> createState() => _profileState();
+}
+
+class _profileState extends State<profile> {
+  String? firstname = '';
+  String? email = '';
+  String? image = '';
+  File? imagmxfile;
+
+  Future _getDataFromDatabase() async {
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((snapshot) async {
+      if (snapshot.exists) {
+        setState(() {
+          firstname = snapshot.data()!["firstname"];
+          email = snapshot.data()!["email"];
+          //image = snapshot.data()!["image"];
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryColor ,
-      appBar: AppBar(title: const Text('Profile',style:
-       TextStyle(fontWeight: FontWeight.bold,color:AppColors.primaryColor),
-       ),
-      backgroundColor:AppColors.secondaryColor) ,
-       
+      backgroundColor: AppColors.primaryColor,
+      appBar: AppBar(
+          title: const Text(
+            'Profile',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: AppColors.primaryColor),
+          ),
+          backgroundColor: AppColors.secondaryColor),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                //for show imagee
+              },
+              child: CircleAvatar(
+                backgroundColor: AppColors.secondaryColor,
+                minRadius: 60.0,
+                child: CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: imagmxfile == null
+                      ? NetworkImage(image!)
+                      : Image.file(imagmxfile!).image,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Name : ' + firstname!,
+                  style: const TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'email : ' + email!,
+              style: const TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
